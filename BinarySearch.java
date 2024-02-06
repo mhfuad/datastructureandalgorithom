f3239a9efac86ce8d43c377e7ad8807d2963d801
mhfuad <mhfuad2@gmail.com>
2024-02-06 12:47:10 +0600

    binary search

---
+++ BinarySearch.java
@@ -0,0 +1,31 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        int res = binary_search(arr, 11);
+        if( res == 99){
+            System.out.println("Number not found");
+        }
+        System.out.println(res);
+    }
+
+    public static int binary_search(int[] arr, int num){
+
+        int left, right, mid;
+        left = 0;
+        right = arr.length -1;
+
+        while (left <= right){
+            mid = (left + right) / 2;
+            if(arr[mid] == num){
+                return mid;
+            }
+
+            if(arr[mid] < num){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return 99;
+    }
+}

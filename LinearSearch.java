f45777277cf4f27d09d6ed00bef57dd8b105630b
mhfuad <mhfuad2@gmail.com>
2024-03-26 15:34:55 +0600

    linear search

--- LinearSearch.java
+++ LinearSearch.java
@@ -2,11 +2,13 @@ public class LinearSearch {
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8,9,10};
         int x = linearSearch(arr, 5);
+        System.out.println(x);
     }
 
     public static int linearSearch(int[] array, int expected){
         int result = 0;
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length ; i++) {
+            System.out.println(array[i]);
             if (array[i] == expected){
                 result = i;
             }

18f4901ca52a5837c77b99859940a9711b88445c
mhfuad <mhfuad2@gmail.com>
2024-04-01 16:26:05 +0600

    selection sort

---
+++ SelectionSort.java
@@ -0,0 +1,11 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+
+    }
+
+    public static int[] section(int[] arr){
+
+        return arr;
+    }
+}

e352d630ea07a143516d4ded75e07fcd551eb19a
mhfuad <mhfuad2@gmail.com>
2024-06-16 02:39:59 +0600

    sort function

---
+++ SelectionSort.java
@@ -0,0 +1,21 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = {5,2,6,3,4};
+
+        for(int i = 0; i <= arr.length - 1; i++){
+            
+        }
+    }
+
+    public static int selectionSearch(int[] array){
+        sort(1,2);
+        return 1;
+    }
+
+    public static int sort(int x, int y){
+        y = x + y;
+        return 0;
+    }
+
+
+}

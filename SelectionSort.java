9cf6a6bf2ac1c972f61eafc7b7e96e64138caf94
mhfuad <mhfuad2@gmail.com>
2024-06-28 18:32:27 +0600

    swap function done

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,21 +1,23 @@
 public class SelectionSort {
     public static void main(String[] args) {
         int arr[] = {5,2,6,3,4};
-
-        for(int i = 0; i <= arr.length - 1; i++){
-
-        }
+        selectionSearch(arr);
+//        for(int i = 0; i <= arr.length - 1; i++){
+//
+//        }
     }
 
     public static int selectionSearch(int[] array){
-        sort(1,2);
+        sort(10,20);
         return 1;
     }
 
     public static int sort(int x, int y){
-        y = x + y;
+        x = x+y;
+        y = x -y;
+        x = x -y;
+        System.out.println(x +" = "+ y);
         return 0;
     }
-
-
+    
 }

b047c30dbfe3c3c50afd1427af6227d6645792de
mhfuad <mhfuad2@gmail.com>
2024-06-30 16:01:36 +0600

    selection sort first loop

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,22 +1,28 @@
+import java.util.Arrays;
+
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {5,2,6,3,4};
-
-        selectionSearch(arr);
+        System.out.println(Arrays.toString(selectionSearch(arr)));;
     }
 
     public static int[] selectionSearch(int[] array){
-        for(int i = 0; i < array.length; i++){
-
+        for(int i = 0; i <= array.length - 2; i++){
+            if(array[i] < array[i+1]){
+                int[] num = {array[i], array[i+1]};
+                int[] swapt = swap(num);
+                array[i] = swapt[0];
+                array[i+1] = swapt[1];
+            }
         }
-        swap(25,50);
         return array;
     }
 
-    public static void swap(int x, int y){
-        x = x + y;
-        y = x - y;
-        x = x - y;
-        System.out.println(x +"="+ y);
+    public static int[] swap(int[] arr){
+        arr[0] = arr[0] + arr[1];
+        arr[1] = arr[0] - arr[1];
+        arr[0] = arr[0] - arr[1];
+        System.out.println(arr[0]+ "="+ arr[1]);
+        return arr;
     }
 }

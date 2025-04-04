9d840c278078b67c8496d6e3495719026e55d8d6
mhfuad <mhfuad2@gmail.com>
2025-04-04 16:49:25 +0600

    Insertion sort

---
+++ InsertionSort.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {5,2,6,3,4,8,11,10,9};
+
+        System.out.println(Arrays.toString(arr));
+
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
